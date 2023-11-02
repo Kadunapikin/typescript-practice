addc561eb878b25c8eab5f094bd679c4e32d8811
kadunapikin <ibakoshi27@gmail.com>
2023-11-02 14:53:32 +0100

    create variableMe.ts file and declare a string type variable

---
+++ 01intro/variableMe.js
@@ -0,0 +1,2 @@
+var greeting = 'Hello Isaac';
+console.log(greeting);
---
+++ 01intro/variableMe.ts
@@ -0,0 +1,9 @@
+let greeting: string = 'Hello Isaac';
+
+let myNum = 6
+
+greeting.toLowerCase
+
+console.log(greeting);
+
+export {}
