4fc72b9bf4726ea79f31d5a629e81b3d7ca20346
kadunapikin <ibakoshi27@gmail.com>
2023-11-02 15:13:26 +0100

    add a boolean type variable to variableMe.ts

--- 01intro/variableMe.ts
+++ 01intro/variableMe.ts
@@ -5,8 +5,11 @@ console.log(greeting);
 
 //Number
 let userId: number = 13927;
-const numberMethod = userId.toLocaleString;
+const numberMethod = userId.toLocaleString();
 console.log(numberMethod);
+
+//boolean
+let isLoggedIn: boolean = false;
 
 
 
