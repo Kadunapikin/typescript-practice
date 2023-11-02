d18aaeea2273dc82a84bda9e1f80ca098b4ba580
kadunapikin <ibakoshi27@gmail.com>
2023-11-02 23:39:52 +0100

    create object.ts and add a function to create a person

---
+++ 01intro/object.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var person = {
+    name: 'Isaac',
+    email: 'isaac@I.com',
+    isActive: true
+};
+function createPerson(_a) {
+    var string = _a.name, boolean = _a.isPaid;
+}
+createPerson({ name: 'Abel', isPaid: false });
---
+++ 01intro/object.ts
@@ -0,0 +1,11 @@
+const person = {
+    name: 'Isaac',
+    email: 'isaac@I.com',
+    isActive: true
+};
+
+function createPerson ({name: string, isPaid: boolean}) {
+}
+createPerson({name: 'Abel', isPaid: false});
+
+export {}
