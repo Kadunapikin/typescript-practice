3f6093014445122c8490037ce3c00ba7a9e02c30
kadunapikin <ibakoshi27@gmail.com>
2023-11-02 21:03:21 +0100

    create a functions.js file and add typescript function

---
+++ 01intro/functions.js
@@ -0,0 +1,5 @@
+function addTwo(num) {
+    return num + 2;
+}
+var plusTwo = addTwo(5);
+console.log(plusTwo);
---
+++ 01intro/functions.ts
@@ -0,0 +1,6 @@
+function addTwo (num: number) {
+    return num + 2
+}
+
+const plusTwo = addTwo(5);
+console.log(plusTwo);
