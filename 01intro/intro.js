c6d3cc4e502530e93ba4d2ce719f2f29524f3fd3
kadunapikin <ibakoshi27@gmail.com>
2023-11-02 14:41:35 +0100

    declare a user variable as an object

---
+++ 01intro/intro.js
@@ -0,0 +1,3 @@
+var user = { name: 'Isaac', age: 32 };
+console.log('user.name');
+console.log('Isaac');
---
+++ 01intro/intro.ts
@@ -0,0 +1,5 @@
+const user = {name: 'Isaac', age: 32}
+
+console.log('user.name');
+console.log('Isaac');
+
