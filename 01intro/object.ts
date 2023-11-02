77a358d5500a735d9d7b5a4da5daad1b451b876c
kadunapikin <ibakoshi27@gmail.com>
2023-11-03 00:56:02 +0100

    add a read only keyword to a type  and try to edit

--- 01intro/object.ts
+++ 01intro/object.ts
@@ -21,14 +21,34 @@
 // const newPet = {name: 'cat', isPaid: false, color: 'white'}; 
 // createPet(newPet);
 
-//Name type alias
+// //Name type alias
+// type User = {
+//     name: string;
+//     email: string;
+//     isActive: boolean
+// }
+
+// function createUser (user: User) {}
+// createUser({name: 'zikist', email: 'zikist@z.com', isActive: false})
+
+
+//Adding the read only keyword
 type User = {
+    readonly _id: string  //when you don't want anyone to have access to this on the data base
     name: string;
     email: string;
     isActive: boolean
 }
 
-function createUser (user: User) {}
-createUser({name: 'zikist', email: 'zikist@z.com', isActive: false})
+let myUser: User = {
+    _id: '1234',
+    name: 'H',
+    email: 'h@h.com',
+    isActive: false
+}
+
+//You can change somethings except for _id
+myUser.email = 'm@m.com';
+// myUser._id = '13927'; // Won't work coz its read only
 
 export {}
