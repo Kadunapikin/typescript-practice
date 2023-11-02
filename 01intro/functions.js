1c48fc90e31272c573775c67e2965d6110d8031b
kadunapikin <ibakoshi27@gmail.com>
2023-11-02 21:13:49 +0100

    add a string type function

--- 01intro/functions.js
+++ 01intro/functions.js
@@ -1,5 +1,12 @@
+//Number function
 function addTwo(num) {
     return num + 2;
 }
 var plusTwo = addTwo(5);
 console.log(plusTwo);
+//string functon
+function getUper(val) {
+    return val.toUpperCase();
+}
+var caps = getUper('masqured');
+console.log(caps);
--- 01intro/functions.ts
+++ 01intro/functions.ts
@@ -1,6 +1,15 @@
+//Number function
 function addTwo (num: number) {
     return num + 2
 }
 
 const plusTwo = addTwo(5);
 console.log(plusTwo);
+
+//string functon
+function getUper(val: string) {
+    return val.toUpperCase()
+}
+ const caps = getUper('masqured');
+ console.log(caps);
+ 
