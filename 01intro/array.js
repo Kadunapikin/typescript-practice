a6019cbece7263e914108945288febc435d02adc
kadunapikin <ibakoshi27@gmail.com>
2023-11-03 10:30:02 +0100

    create array.ts file and add an array number type variable

--- 01intro/array.js
+++ 01intro/array.js
@@ -1,6 +1,9 @@
 "use strict";
-//creating an array in typescript
 Object.defineProperty(exports, "__esModule", { value: true });
+//creating an array in typescript
 var superHeroes = [];
+var heroePower = [];
 superHeroes.push('superman', 'ironman');
+heroePower.push(2, 3, 4);
 console.log(superHeroes);
+console.log(heroePower);
--- 01intro/array.ts
+++ 01intro/array.ts
@@ -1,9 +1,12 @@
 //creating an array in typescript
-
 const superHeroes: string[] = [];
+const heroePower: number[] = [];
 
 superHeroes.push('superman', 'ironman')
+heroePower.push(2,3,4);
 console.log(superHeroes);
+console.log(heroePower);
+
 
 
 
