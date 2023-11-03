7f5a25233da590b823acf2d7cda7a3d6b52a06fa
kadunapikin <ibakoshi27@gmail.com>
2023-11-03 12:18:14 +0100

    add a tupel to a type variable

--- 01intro/tupEnums.ts
+++ 01intro/tupEnums.ts
@@ -4,4 +4,11 @@ let Tuser: (string | number | boolean)[] = [1,'5', true]; //this can be written 
 let tUser: [string, boolean, number]
 tUser = ['Isaac', false, 454]  //if you switch the arrangement youll get an error message
 
+//using tupel on rgb
+let rgb: [number, number, number] = [255,255,255];
+
+//using a tupel on type
+type User = [number, string]
+const newUser: User = [244, 'i@i.com']
+
 export {}
