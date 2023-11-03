e6c5b70694163edb7531b2a428acb3e2f3842938
kadunapikin <ibakoshi27@gmail.com>
2023-11-03 12:07:45 +0100

    create tupEnums.ts file and add a tupel variable

---
+++ 01intro/tupEnums.ts
@@ -0,0 +1,7 @@
+let Tuser: (string | number | boolean)[] = [1,'5', true]; //this can be written without following the format of the type
+
+//working with tupels
+let tUser: [string, boolean, number]
+tUser = ['Isaac', false, 454]  //if you switch the arrangement youll get an error message
+
+export {}
