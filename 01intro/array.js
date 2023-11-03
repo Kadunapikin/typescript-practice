5cfe459949a6077c3581eff633fad6d5575018f7
kadunapikin <ibakoshi27@gmail.com>
2023-11-03 10:27:04 +0100

    create array.ts file and add an array string type variable

---
+++ 01intro/array.js
@@ -0,0 +1,6 @@
+"use strict";
+//creating an array in typescript
+Object.defineProperty(exports, "__esModule", { value: true });
+var superHeroes = [];
+superHeroes.push('superman', 'ironman');
+console.log(superHeroes);
---
+++ 01intro/array.ts
@@ -0,0 +1,10 @@
+//creating an array in typescript
+
+const superHeroes: string[] = [];
+
+superHeroes.push('superman', 'ironman')
+console.log(superHeroes);
+
+
+
+export {}
