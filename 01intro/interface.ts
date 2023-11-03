2c9754bfd87c59d413ff5c8208b6e2e8821c05b8
kadunapikin <ibakoshi27@gmail.com>
2023-11-03 14:54:43 +0100

    add readonly keyword, startTrial method and getCoupon method to User interface

--- 01intro/interface.ts
+++ 01intro/interface.ts
@@ -4,6 +4,15 @@ interface User {
     email: string,
     userId: number
     googleId?: number
+    startTrial: () => string
+    getCoupon (couponName: string, value: number): number
 }
 
-const Izo: User = {dbId: 22, email: 'i@i.com', userId: 13927}
+const Izo: User = {dbId: 22, email: 'i@i.com', userId: 13927,
+startTrial: () => {
+    return 'Trial started'
+},
+getCoupon: (name: 'Iceman', off: 15) => {
+    return 15
+}
+}
