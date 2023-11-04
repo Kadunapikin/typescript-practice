47e4f80399a600bed9531bab50784f6db1d56099
kadunapikin <ibakoshi27@gmail.com>
2023-11-04 07:28:37 +0100

    create an Admin interface and extends it to use properties of User interface

--- 01intro/interface.ts
+++ 01intro/interface.ts
@@ -8,6 +8,7 @@ interface User {
     getCoupon (couponName: string, value: number): number
 }
 
+
 const Izo: User = {dbId: 22, email: 'i@i.com', userId: 13927,
 startTrial: () => {
     return 'Trial started'
@@ -20,4 +21,18 @@ getCoupon: (name: 'Iceman', off: 15) => {
 //Reopning the interface
 interface User {  //you can add more id to already created interface
     githubToken: string
+}
+
+// extending an interface
+interface Admin extends User {
+    role: 'admin' | 'leaner' | 'instructor'
+}
+
+const Masku: Admin = {dbId: 22, email: 'i@i.com', userId: 13927,
+startTrial: () => {
+    return 'Trial started'
+}, githubToken: '',
+getCoupon: (name: 'Iceman', off: 15) => {
+    return 15
+}, role: 'admin'
 }
