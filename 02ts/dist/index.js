501b242e81d375b466435adabcba52a1412e0d42
kadunapikin <ibakoshi27@gmail.com>
2023-11-05 10:55:27 +0100

    add a shorter method to write class codes

--- 02ts/dist/index.js
+++ 02ts/dist/index.js
@@ -2,14 +2,27 @@
 console.log("typescript is still here");
 console.log("typescript is amazing");
 //Creating a class
+// class User {
+//     private email: string
+//     name: string
+//     city: string = ''  //if you're adding a city then you need to initialize it
+//     public age: number
+//     constructor (email: string, name: string, age: number) {
+//         this.email = email;
+//         this.name = name;
+//         this.age = age
+//     }
+// }
+//A shorter way to write thesame code
 class User {
-    constructor(email, name, age) {
-        this.city = ''; //if you're adding a city then you need to initialize it
+    constructor(email, name, age, userId) {
         this.email = email;
         this.name = name;
         this.age = age;
+        this.userId = userId;
+        this.city = ''; //if you're adding a city then you need to initialize it
     }
 }
-const Isaac = new User("i@i.com", "isaac", 32);
+const Isaac = new User("i@i.com", "isaac", 32, "123");
 Isaac.city = "kadaCity"; //I can Have access to the city
 Isaac.age; //Only public properties and the ones not assigned the private keywords can be accessed outside the class
--- 02ts/src/index.ts
+++ 02ts/src/index.ts
@@ -2,18 +2,29 @@ console.log("typescript is still here");
 console.log("typescript is amazing");
 
 //Creating a class
+// class User {
+//     private email: string
+//     name: string
+//     city: string = ''  //if you're adding a city then you need to initialize it
+//     public age: number
+//     constructor (email: string, name: string, age: number) {
+//         this.email = email;
+//         this.name = name;
+//         this.age = age
+//     }
+// }
+
+//A shorter way to write thesame code
 class User {
-    private email: string
-    name: string
     city: string = ''  //if you're adding a city then you need to initialize it
-    public age: number
-    constructor (email: string, name: string, age: number) {
-        this.email = email;
-        this.name = name;
-        this.age = age
+    constructor (
+        public email: string, 
+        public name: string, 
+        public age: number,
+        private userId: string) {
     }
 }
 
-const Isaac = new User("i@i.com", "isaac", 32);
+const Isaac = new User("i@i.com", "isaac", 32, "123");
 Isaac.city = "kadaCity"  //I can Have access to the city
 Isaac.age  //Only public properties and the ones not assigned the private keywords can be accessed outside the class
