c93277d10200bf5b61a111ac66bbd43a95e79f0a
kadunapikin <ibakoshi27@gmail.com>
2023-11-05 09:40:25 +0100

    add a User class to index.ts

--- 02ts/dist/index.js
+++ 02ts/dist/index.js
@@ -1,3 +1,10 @@
 "use strict";
 console.log("typescript is still here");
 console.log("typescript is amazing");
+//Creating a class
+class User {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+    }
+}
--- 02ts/src/index.ts
+++ 02ts/src/index.ts
@@ -1,2 +1,12 @@
 console.log("typescript is still here");
 console.log("typescript is amazing");
+
+//Creating a class
+class User {
+    email: string
+    name: string
+    constructor (email: string, name: string) {
+        this.email = email;
+        this.name = name
+    }
+}
