6fc10f9034817c907a0d05ae9c4980477f5842e7
kadunapikin <ibakoshi27@gmail.com>
2023-11-05 10:08:33 +0100

    add a public and private key word

--- 02ts/dist/index.js
+++ 02ts/dist/index.js
@@ -3,11 +3,13 @@ console.log("typescript is still here");
 console.log("typescript is amazing");
 //Creating a class
 class User {
-    constructor(email, name) {
+    constructor(email, name, age) {
         this.city = ''; //if you're adding a city then you need to initialize it
         this.email = email;
         this.name = name;
+        this.age = age;
     }
 }
-const Isaac = new User("i@i.com", "isaac");
+const Isaac = new User("i@i.com", "isaac", 32);
 Isaac.city = "kadaCity"; //I can Have access to the city
+Isaac.age; //Only public properties and the ones not assigned the private keywords can be accessed outside the class
--- 02ts/src/index.ts
+++ 02ts/src/index.ts
@@ -3,14 +3,17 @@ console.log("typescript is amazing");
 
 //Creating a class
 class User {
-    email: string
+    private email: string
     name: string
     city: string = ''  //if you're adding a city then you need to initialize it
-    constructor (email: string, name: string) {
+    public age: number
+    constructor (email: string, name: string, age: number) {
         this.email = email;
-        this.name = name
+        this.name = name;
+        this.age = age
     }
 }
 
-const Isaac = new User("i@i.com", "isaac");
+const Isaac = new User("i@i.com", "isaac", 32);
 Isaac.city = "kadaCity"  //I can Have access to the city
+Isaac.age  //Only public properties and the ones not assigned the private keywords can be accessed outside the class
