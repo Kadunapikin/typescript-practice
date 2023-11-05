89bfccce936f6df3c5c9f55e630738cdf49a5455
kadunapikin <ibakoshi27@gmail.com>
2023-11-05 09:55:20 +0100

    add a city parameter and access it

--- 02ts/dist/index.js
+++ 02ts/dist/index.js
@@ -4,7 +4,10 @@ console.log("typescript is amazing");
 //Creating a class
 class User {
     constructor(email, name) {
+        this.city = ''; //if you're adding a city then you need to initialize it
         this.email = email;
         this.name = name;
     }
 }
+const Isaac = new User("i@i.com", "isaac");
+Isaac.city = "kadaCity"; //I can Have access to the city
--- 02ts/src/index.ts
+++ 02ts/src/index.ts
@@ -5,8 +5,12 @@ console.log("typescript is amazing");
 class User {
     email: string
     name: string
+    city: string = ''  //if you're adding a city then you need to initialize it
     constructor (email: string, name: string) {
         this.email = email;
         this.name = name
     }
 }
+
+const Isaac = new User("i@i.com", "isaac");
+Isaac.city = "kadaCity"  //I can Have access to the city
