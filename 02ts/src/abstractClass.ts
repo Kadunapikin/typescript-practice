6103aaf16a40bcb04a6490eaa7e9e2e672be56c8
kadunapikin <ibakoshi27@gmail.com>
2023-11-05 19:30:20 +0100

    add abstract keywork to TakePhoto class, extends it to Youtube class and create an object from Youtube instance

--- 02ts/src/abstractClass.ts
+++ 02ts/src/abstractClass.ts
@@ -1,9 +1,22 @@
-class TakePhoto {
+abstract class TakePhoto {   //As soon as abstract is added, it becomes impossible to create an object from the abstract class
     constructor(
         public cameraMode: string,
         public shots: number
     ) {}
 }
 
-//creating an object from the class
-const Ice = new TakePhoto("zikist", 10)
+//creating an object from the class before abstract was added to it
+// const Ice = new TakePhoto("zikist", 10)
+
+class Youtube extends TakePhoto {
+    constructor(
+        public cameraMode: string,
+        public shots: number,
+        public filter: string
+    ) {
+        super(cameraMode, shots)  //You need to add this super before it stops giving you error
+    }
+}
+
+//creating an object from the class after abstract was added to it. New objects can be created from classes that extends an abstract class
+const Ice = new Youtube ("zikist", 10, "")
