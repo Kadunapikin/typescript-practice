fcc9fe33afb3510d146ffdc30fba95bab9c2d413
kadunapikin <ibakoshi27@gmail.com>
2023-11-07 11:09:15 +0100

    create a function to show show in operator narrowing

--- 02ts/src/detection.ts
+++ 02ts/src/detection.ts
@@ -13,3 +13,22 @@ function provideId(id: string | null) {
     id.toLowerCase
 }
 
+
+//The in operator narrowing
+interface User {
+    name: string
+    email: string
+}
+
+interface Admin {
+    name: string
+    email: string
+    isAdmin: boolean
+}
+
+function isAdminAccount (account: User | Admin) {
+    if ("isAdmin" in account) {
+        return account.isAdmin
+    }
+    return
+}
